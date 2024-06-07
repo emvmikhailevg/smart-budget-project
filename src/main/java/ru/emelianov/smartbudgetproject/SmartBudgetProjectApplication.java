4c25222eaac582e0a50d4fb96f3f0b42cdc85c7b
mikhailemv <emelianovmkevg@yandex.ru>
2024-06-07 12:35:33 +0500

    Initial Commit

---
+++ src/main/java/ru/emelianov/smartbudgetproject/SmartBudgetProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.emelianov.smartbudgetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBudgetProjectApplication.class, args);
+    }
+
+}

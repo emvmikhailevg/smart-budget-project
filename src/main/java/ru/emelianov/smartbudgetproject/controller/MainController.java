b79515a0c5ef6be1dd8efd6d0acdf69d1e514a11
mikhailemv <emelianovmkevg@yandex.ru>
2024-06-09 19:16:31 +0500

    Сделана страница входа

---
+++ src/main/java/ru/emelianov/smartbudgetproject/controller/MainController.java
@@ -0,0 +1,16 @@
+package ru.emelianov.smartbudgetproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String getMainPage() {
+        return "main";
+    }
+
+}
